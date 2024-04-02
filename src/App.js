2826d33465ed659e10592f4dcfec56b7cb5fa227
ggang89 <ggang89@gmail.com>
2024-04-03 02:35:19 +0900

    한 칸이 클릭됐을 때 부모 컴포넌트가 어떤 칸인지 알기위해서 자식 컴포넌트가 부모의 state를 업데이트할 수 있도록 함수를 만들어서 prop으로 연결

--- src/App.js
+++ src/App.js
@@ -2,18 +2,29 @@ import "./App.css";
 import { useState } from "react";
 import React from "react";
 
-function Square({ value }) {
-  return <button className="square">{value}</button>;
+function Square({ value, onSquareClick }) {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
 }
 
 function App() {
   const [squares, setSquares] = useState(Array(9).fill(null)); //9개의 엘리먼트를 만들고 그 안에 null삽입
+ 
+  function handleClick() {
+    const nextSquares = squares.slice();
+    nextSquares[0] = "X";
+    setSquares(nextSquares);
+  }
+
   return (
     <>
       <div className="board-row">
-        <Square value={squares[0]} />
-        <Square value={squares[1]} />
-        <Square value={squares[2]} />
+        <Square value={squares[0]} onSquareClick={handleClick}/>
+        <Square value={squares[1]} onSquareClick={handleClick}/>
+        <Square value={squares[2]} onSquareClick={handleClick}/>
       </div>
       <div className="board-row">
         <Square value={squares[3]} />
