74a525fc346b0c4d23c432ef515a25eea0759d13
ggang89 <ggang89@gmail.com>
2024-04-03 01:49:29 +0900

    기본형태(버튼9개)만들고, useState로 각 버튼에 값 연결

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Tic -Tac -Toe 앱 만들기
 #### 리액트 공식문서 안의 앱만들기
+
+[리액트 공식문서 번역](https://react-ko.dev/learn/tutorial-tic-tac-toe)
--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
+import "./App.css";
+import { useState } from "react";
+import React from "react";
 
-import './App.css';
+function Square() {
+  const [value, setValue] = useState(null);
+  function handleClick() {
+    setValue("X");
+  }
+  return (
+    <button className="square" onClick={handleClick}>
+      {value}
+    </button>
+  );
+}
 
 function App() {
   return (
-    <div >
-      
-    </div>
+    <>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </>
   );
 }
 
