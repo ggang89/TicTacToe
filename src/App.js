a41abb9e2097f537e088ee5a0d251ba8d6b47f51
ggang89 <ggang89@gmail.com>
2024-04-05 23:18:44 +0900

    button을 누르면 x가 나오도록 hadleClick함수에 인덱스 번호 추가함

--- src/App.js
+++ src/App.js
@@ -12,29 +12,29 @@ function Square({ value, onSquareClick }) {
 
 function App() {
   const [squares, setSquares] = useState(Array(9).fill(null)); //9개의 엘리먼트를 만들고 그 안에 null삽입
- 
-  function handleClick() {
+
+  function handleClick(i) {
     const nextSquares = squares.slice();
-    nextSquares[0] = "X";
+    nextSquares[i] = "X";
     setSquares(nextSquares);
   }
 
   return (
     <>
       <div className="board-row">
-        <Square value={squares[0]} onSquareClick={handleClick}/>
-        <Square value={squares[1]} onSquareClick={handleClick}/>
-        <Square value={squares[2]} onSquareClick={handleClick}/>
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
       </div>
       <div className="board-row">
-        <Square value={squares[3]} />
-        <Square value={squares[4]} />
-        <Square value={squares[5]} />
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
       </div>
       <div className="board-row">
-        <Square value={squares[6]} />
-        <Square value={squares[7]} />
-        <Square value={squares[8]} />
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </>
   );
